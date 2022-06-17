533139e013ce8fbcb7ca8646d1eff411daf03240
Amandeep <singh.amandeep5665@gmail.com>
2022-06-17 17:00:34 +0530

    added card

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ const {src,dest,watch,series} = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function buildStyles(){
-    return src('index.scss')
+    return src('obelus/**/*.scss')
         .pipe(sass())
         .pipe(dest('css'))
 }
 
 function watchSass(){
-    watch(['index.scss'], buildStyles)
+    watch(['obelus/**/*.scss'], buildStyles)
 }
 
 
-exports.default = series(buildStyles,watchSass)
+exports.default = series(buildStyles,watchSass) 
